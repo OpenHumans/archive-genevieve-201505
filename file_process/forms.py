a6b6da841a30c252dc5d9a1c3939625ed9facdce
Emily Salvador <emilysa@mit.edu>
2014-07-30 14:47:47 +0000

    added limit on file upload

--- file_process/forms.py
+++ file_process/forms.py
@@ -9,13 +9,15 @@ class UploadForm(forms.Form):
     )
 
     
-    #Sets the max size for file upload
+    #Must be gzipped and under Max_Upload_Size
     def clean_uploadfile(self):
         content = self.cleaned_data['uploadfile']
         print "Content Size =",
         print content.size
         if content.size > int(settings.MAX_UPLOAD_SIZE):
             raise forms.ValidationError('Please keep filesize under max size')
+        if not content.name.endswith('.gz'):
+            raise forms.ValidationError('Please upload a file')
         return content
             
             
