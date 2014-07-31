a000ff7f462704318e63b08c3cff6655752517c9
Emily Salvador <emilysa@mit.edu>
2014-07-31 20:55:53 +0000

    merge conflict

--- login/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
-
-from . import views
-
-urlpatterns = patterns('login.views',
-    url(r'^$', 'home', name='home'),
-    )
--- login/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-
-def home(request):
-    return HttpResponse("Hello, Welcome to Genevieve.")
