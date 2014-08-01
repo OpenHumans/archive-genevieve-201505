377f4b52d191b58cbef83ca305f6c55474c50039
Emily Salvador <emilysa@mit.edu>
2014-08-01 14:45:58 +0000

    added topbar and homepage

--- genevieve/urls.py
+++ genevieve/urls.py
@@ -16,13 +16,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'genevieve.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'genevieve.views.home', name='home'),
     (r'^file_process/', include('file_process.urls')),
-    url(r'^signup/$', UserCreateView.as_view()),
+    url(r'^accounts/signup/$', UserCreateView.as_view()),
     url(r'^login/$', auth_views.login,
         {'template_name': 'login.html',
          'extra_context': {'next': '/file_process'}}, name='auth_login'),
-    url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/file_process'},
+    url(r'^logout/$', auth_views.logout, {'next_page': '/file_process'},
         name='auth_logout'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- genevieve/views.py
+++ genevieve/views.py
@@ -1,9 +1,15 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
 
+def home(request):
+     return render_to_response(
+         'home.html'
+         )
 class UserCreateView(CreateView):
     """
     A view that creates a new user, logs them in, and redirects them to the
