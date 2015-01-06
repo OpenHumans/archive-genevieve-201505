39ed66d6b1aa0fdd04bbd96e3942fdbbb910b490
Emily Salvador <emilysa@mit.edu>
2015-01-06 22:32:42 +0000

    added minor js

--- genevieve/settings_local_example.py
+++ genevieve/settings_local_example.py
@@ -2,7 +2,7 @@
 
 # You MUST define the secret key, and make it unique for your own copy.
 # One way is to run md5sum on a long randomly produced paragraph of text.
-SECRET_KEY = 'f791c878c376cb54c3a28304d3342fe8f791c878c376cb54c3a28304d3342fe8'
+SECRET_KEY = 'f791c878c376cb54c3a28304jon32rnfl931xc8304d3342fe8'
 
 # You'll also need to set the redirect uri in your 23andme API account to
 # the correct path and port your app is listening on, eg. (for local dev):
