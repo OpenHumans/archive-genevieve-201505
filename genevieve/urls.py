c8757c2dbdefcc02dc41063b0fd4f8c43607fbc8
Emily Salvador <emilysa@mit.edu>
2015-01-12 17:07:03 +0000

    still broken, creating an issues page

--- genevieve/urls.py
+++ genevieve/urls.py
@@ -5,11 +5,25 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 from .views import UserCreateView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+
+    #simple pages
+    url(r'^$', TemplateView.as_view(template_name='pages/home.html'),
+        name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'),
+        name='about'),
+    url(r'^community_guidelines/$',
+        TemplateView.as_view(template_name='pages/community_guidelines.html'),
+        name='community_guidelines'),
+    url(r'^contact-us/$',
+        TemplateView.as_view(template_name='pages/contact_us.html'),
+        name='contact_us'),
 
     url(r'^admin/', include(admin.site.urls)),
 
