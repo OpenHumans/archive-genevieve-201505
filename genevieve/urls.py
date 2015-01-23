4342a898cc9c03773f41d1992c644a0a31f21b1e
Emily Salvador <emilysa@Emilys-MacBook-Pro.local>
2015-01-23 09:20:09 -0500

    login button still doesn't work, added sign-up errors, removed duplicate code

--- genevieve/urls.py
+++ genevieve/urls.py
@@ -2,10 +2,6 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-
-from .views import UserCreateView
-from django.core.urlresolvers import reverse_lazy
 from django.views.generic import TemplateView
 
 admin.autodiscover()
@@ -13,8 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
 
-    #simple pages
-    url(r'^$', TemplateView.as_view(template_name='home.html'),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
 
     url(r'^admin/', include(admin.site.urls)),
 
