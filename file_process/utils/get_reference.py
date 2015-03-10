d68f391d15927db65ea4e62d67bd9faf37b5deaf
Madeleine Ball <mpball@gmail.com>
2015-03-10 02:33:00 -0400

    Return N for ref allele in pos uncovered by ref
    This arose during processing of a CGI var file originating from LFR
    data (not sure, but LFR is an advanced technique, so it's possible CGI
    is calling some positions that aren't called in reference).

--- file_process/utils/get_reference.py
+++ file_process/utils/get_reference.py
@@ -5,5 +5,8 @@ from .twobit import TwoBitFile
 def get_reference_allele(chrom, start, hg19_path):
     twobit_file = TwoBitFile(hg19_path)
     end = start + 1
-    refallele = twobit_file[chrom][start:end]
+    try:
+        refallele = twobit_file[chrom][start:end]
+    except TypeError:
+        refallele = 'N'
     return refallele
