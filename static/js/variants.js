3d61b632c10b55f43969e811dd25072e665f9501
Emily Salvador <emilysa@dhcp-18-111-109-124.dyn.mit.edu>
2015-05-18 11:07:12 -0400

    added search and refresh page

--- static/js/variants.js
+++ static/js/variants.js
@@ -19,12 +19,12 @@ var createClnvarRow = function(clnvarEntry, varData, idxEntry, idxVar) {
   var clinvarDataTemplateHTML = $("#clinvar-data-template").html();
   var templateClinvarData = _.template(clinvarDataTemplateHTML);
   return templateClinvarData({
+    clnvarEntryCondition: cleanedCondition,
     clinvarDataId: "clinvar-data-" + idxEntry,
     variantDataClass: "genome-variant-" + idxVar,
     clnvarClnsig: clnvarEntry.clnsig,
     clnvarEntryURL: "http://www.ncbi.nlm.nih.gov/clinvar/" +
                     clnvarEntry.accnum,
-    clnvarEntryCondition: cleanedCondition,
     // TODO: file_process does not seem like the correct place for this!
     commentaryURL: "/file_process/commentary/" + clnvarEntry.id,
     exacURL: "http://exac.broadinstitute.org/variant/" +
@@ -46,11 +46,10 @@ var getData = function() {
     $("#genome-variants-table")
     .html('');
     for (var idx in genomeVars) {
-
       // Create and add row for Variant data.
+
       var $rowVariant = createVariantRow(genomeVars[idx], idx);
       $("#genome-variants-table").append($rowVariant);
-
       // Create and add a row for each ClinVar record.
       var clinvarrecords = genomeVars[idx].variant_data.clinvarrecords_dataset;
       for (var idx2 in clinvarrecords) {
@@ -59,15 +58,12 @@ var getData = function() {
                                          genomeVars[idx].variant_data,
                                          idx2, idx);
         $("#genome-variants-table").append($rowClnvar);
-
       }
     }
+    $('.table').filterTable();
   });
 }
 
 $(function() {
   getData();
-  setInterval(function() {
-  getData();
-  }, 3000);
 });
